9ad0e2be8952e1d0760d24a95040805abf00a10c
nickstagl <nick.stagl@kellogg.northwestern.edu>
2026-01-20 14:16:44 +0000

    Airbnb Practice Lab 6

--- practice-6.sql
+++ practice-6.sql
@@ -15,6 +15,6 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-SELECT listings.neighborhood, reviews.COUNT
-FROM reviews INNER JOIN listings ON revies.id = listings.id
+SELECT listings.neighborhood, COUNT(reviews.listing_id)
+FROM reviews INNER JOIN listings ON reviews.listing_id = listings.id
 GROUP BY listings.neighborhood;
